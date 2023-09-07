d97065a0926766efab183916b01e708b27a228e9
Varsha Das <vdaskol@gmail.com>
2023-09-07 10:43:13 +0530

    comparator

--- ComparatorsDemo/ComparatorsUsecases.java
+++ ComparatorsDemo/ComparatorsUsecases.java
@@ -23,78 +23,82 @@ public class ComparatorsUsecases {
        Case-Insensitive Sorting
 
         Reverse Order Sorting
-         */
+        */
 
         // 1. Sort the strings based on their length in ascending order
 
         List<String> strings = Arrays.asList("apples", "elderberry", "banana", "cherry", "date");
         String[] fruits = {"apple", "banana", "cherry", "date"};
-        Collections.sort(strings);
+        Collections.sort(strings, Comparator.comparingInt(String::length));
 
         Arrays.sort(fruits, Comparator.comparingInt(String::length));
 
-        // 2.Sort the list of integers in descending order and print the result
-        List<Integer> list = Arrays.asList(3, 4, 9, 12);
+        System.out.println(Arrays.toString(fruits));
 
-        list.sort(Comparator.reverseOrder());
-        Collections.sort(list,Collections.reverseOrder());
-        System.out.println(list);
-
-
-        // 3.Sort the list of employees based on their age in ascending order and print the result
-
-
-
-
-        List<Employee> employees = Arrays.asList(new Employee(1, "Varsha", 28, 3000),
-                new Employee(2, "Harsha", 21, 4000),
-                new Employee(3, "Tony", 21, 1000),
-                new Employee(4, "Ramesh", 30, 5000));
-
-
-        employees.sort(Comparator.comparingInt(Employee::getAge));
-
-        System.out.println("employees "+employees);
-
-        // 4.Sort the list of employees based on their age in ascending order. If the ages are the same, compare by salary. Print the result.
-//        employees.sort(Comparator.comparingInt(Employee::getAge)
-//                .thenComparingLong(Employee::getSalary));
-//        System.out.println(employees);
-//
-        // 5.Sort the list of strings based on the index of the first occurrence of "e" in each string and print the result
-        strings.sort(Comparator.comparing(str -> str.indexOf("e")));
         System.out.println(strings);
 
+        // 2.Sort the list of integers in descending order and print the result
+//        List<Integer> list = Arrays.asList(3, 4, 9, 12);
+//
+//        list.sort(Comparator.reverseOrder());
+//        Collections.sort(list,Collections.reverseOrder());
+//        System.out.println(list);
+//
+//
+//        // 3.Sort the list of employees based on their age in ascending order and print the result
 //
 //
 //
 //
-//        //6. Sort a list of strings ignoring case sensitivity using a case-insensitive comparator.
+//        List<Employee> employees = Arrays.asList(new Employee(1, "Varsha", 28, 3000),
+//                new Employee(2, "Harsha", 21, 4000),
+//                new Employee(3, "Tony", 21, 1000),
+//                new Employee(4, "Ramesh", 30, 5000));
 //
-//        List<String> strings1 = Arrays.asList("apple", "Banana", "cherry", "Date");
 //
-//        Comparator<String> caseInsensitiveComparator = String.CASE_INSENSITIVE_ORDER;
+//        employees.sort(Comparator.comparingInt(Employee::getAge));
 //
-//        Collections.sort(strings1, caseInsensitiveComparator);
+//        System.out.println("employees "+employees);
 //
-//        //7. Sort a list of dates in ascending order using the comparing() method with a lambda expression.
-//        List<LocalDate> dates = Arrays.asList(
-//                LocalDate.of(2023, 5, 10),
-//                LocalDate.of(2023, 3, 15),
-//                LocalDate.of(2023, 7, 1)
-//        );
-//
-//        Comparator<LocalDate> ascendingComparator = Comparator.comparing(date -> date);
-//
-//        Collections.sort(dates, ascendingComparator);
-//
-//        //8.Sort a list of strings based on the count of a specific character in each string using a custom comparator.
-//
-////        List<String> strings = Arrays.asList("apple", "banana", "cherry", "date", "elderberry");
+//        // 4.Sort the list of employees based on their age in ascending order. If the ages are the same, compare by salary. Print the result.
+////        employees.sort(Comparator.comparingInt(Employee::getAge)
+////                .thenComparingLong(Employee::getSalary));
+////        System.out.println(employees);
 ////
-////        Comparator<String> characterCountComparator = Comparator.comparingInt(str -> StringUtils.countMatches(str, 'e'));
+//        // 5.Sort the list of strings based on the index of the first occurrence of "e" in each string and print the result
+//        strings.sort(Comparator.comparing(str -> str.indexOf("e")));
+//        System.out.println(strings);
+//
 ////
-////        Collections.sort(strings, characterCountComparator);
+////
+////
+////
+////        //6. Sort a list of strings ignoring case sensitivity using a case-insensitive comparator.
+////
+////        List<String> strings1 = Arrays.asList("apple", "Banana", "cherry", "Date");
+////
+////        Comparator<String> caseInsensitiveComparator = String.CASE_INSENSITIVE_ORDER;
+////
+////        Collections.sort(strings1, caseInsensitiveComparator);
+////
+////        //7. Sort a list of dates in ascending order using the comparing() method with a lambda expression.
+////        List<LocalDate> dates = Arrays.asList(
+////                LocalDate.of(2023, 5, 10),
+////                LocalDate.of(2023, 3, 15),
+////                LocalDate.of(2023, 7, 1)
+////        );
+////
+////        Comparator<LocalDate> ascendingComparator = Comparator.comparing(date -> date);
+////
+////        Collections.sort(dates, ascendingComparator);
+////
+////        //8.Sort a list of strings based on the count of a specific character in each string using a custom comparator.
+////
+//////        List<String> strings = Arrays.asList("apple", "banana", "cherry", "date", "elderberry");
+//////
+//////        Comparator<String> characterCountComparator = Comparator.comparingInt(str -> StringUtils.countMatches(str, 'e'));
+//////
+//////        Collections.sort(strings, characterCountComparator);
 
 
     }
--- LambdasAndStreams/src/main/java/StreamsAllCodeSolutions.java
+++ LambdasAndStreams/src/main/java/StreamsAllCodeSolutions.java
@@ -1,113 +1,173 @@
-import java.util.*;
-import java.util.stream.Collectors;
-
-public class StreamsAllCodeSolutions {
-    public static void main(String[] args) {
-        List<Integer> nums = Arrays.asList(1,2,3,4,5,6,8,101);
-        int[] arr = {1,2,3,4};
-       // Write a program to find the sum of all elements in a List using streams.
-        int sums = nums.stream().mapToInt(Integer::intValue).sum();
-        System.out.println(sums);
-
-
-        //Given a List of integers, write a program to find the maximum element using streams.
-        int max = nums.stream().max(Comparator.naturalOrder()).get();
-
-        System.out.println("max element :"+max);
-
-        List<String> fruits = Arrays.asList("apple", "banana", "cherry", "coconut", "apple");
-
-        //Given a List of strings, write a program to count the number of strings that start with a specific character using streams.
-        long fruitCount = fruits.stream().filter( s -> s.startsWith("c")).count();
-        System.out.println("fruitCount: "+fruitCount);
-
-
-        //Write a program to convert a List of strings to uppercase using streams.
-        fruits.stream().map(String::toUpperCase).forEach(System.out::println);
-
-        //Given a List of integers, write a program to filter out the even numbers using streams.
-        // Count the number of elements in a list that satisfy a specific condition using streams.
-
-        nums.stream().filter(n -> n % 2 == 0).forEach(System.out::println);
-        int even = (int) nums.stream().filter(n -> n % 2 == 0).count();
-        System.out.println("no of even "+even);
-
-
-        //Write a program to find the average of a List of floating-point numbers using streams.
-        List<Double> numsFloat = Arrays.asList(1.3, 2.4, 3.1, 4.2, 5.4);
-        double avg = numsFloat.stream().mapToDouble(Double::doubleValue).average().getAsDouble();
-        System.out.println("avg ::"+avg);
-
-        //Given a List of strings, write a program to concatenate all the strings using streams.
-        String concat = fruits.stream().collect(Collectors.joining());
-        System.out.println(concat);
-
-        //Write a program to remove duplicate elements from a List using streams.
-        fruits.stream().distinct().forEach(System.out::println);
-
-        //Given a List of objects, write a program to sort the objects based on a specific attribute using streams.
-        List<Person> people = Arrays.asList(
-                new Person("Alice", 21),
-                new Person("Anna", 29),
-                new Person("Bob", 30),
-                new Person("Barbie", 56),
-                new Person("Charlie", 20)
-        );
-
-        people.stream().sorted(Comparator.comparing(Person::getAge)).collect(Collectors.toList()).forEach(System.out::println);
-
-        //Write a program to check if all elements in a List satisfy a given condition using streams.
-        List<Integer> numbers = Arrays.asList(2, 4, 6, 8, 10);
-        boolean allEven = numbers.stream().allMatch(n -> n % 2 == 0);
-        System.out.println("All numbers are even: " + allEven);
-
-
-        //Sort a list of strings in ascending order using streams.
-        fruits.stream().sorted().collect(Collectors.toList()).forEach(System.out::println);
-
-        //Check if a list contains a specific element using streams.
-        boolean exists = nums.stream().anyMatch(n -> n.equals(9));
-        System.out.println(exists);
-
-        //Create a new list containing the square of each element from the original list using streams
-        nums.stream().map(n -> n*n).collect(Collectors.toList()).forEach(System.out::println);
-
-        //Find the average length of strings in a list using streams.
-        fruits.stream().mapToInt(String::length).average();
-
-        //Find the longest string in a list using streams.
-        fruits.stream().mapToInt(String::length).max();
-
-//        Group a list of objects based on a specific attribute using streams.
-//        List<Employee> sortedEmployees = people.stream()
-//                .sorted(Comparator.comparing(Employee::getDepartment)
-//                        .thenComparing(Employee::getSalary))
+//import java.util.*;
+//import java.util.function.Function;
+//import java.util.stream.Collectors;
+//
+//public class StreamsAllCodeSolutions {
+//    public static void main(String[] args) {
+//        List<Integer> nums = Arrays.asList(1,2,3,4,5,6,8,101);
+//        int[] arr = {1,2,3,4};
+//       // Write a program to find the sum of all elements in a List using streams.
+//        int sums = nums.stream().mapToInt(Integer::intValue).sum();
+//        System.out.println(sums);
+//
+//
+//        //Given a List of integers, write a program to find the maximum element using streams.
+//        int max = nums.stream().max(Comparator.naturalOrder()).get();
+//
+//        System.out.println("max element :"+max);
+//
+//        List<String> fruits = Arrays.asList("apple", "banana", "cherry", "coconut", "apple");
+//
+//        //Given a List of strings, write a program to count the number of strings that start with a specific character using streams.
+//        long fruitCount = fruits.stream().filter( s -> s.startsWith("c")).count();
+//        System.out.println("fruitCount: "+fruitCount);
+//
+//
+//        //Write a program to convert a List of strings to uppercase using streams.
+//        fruits.stream().map(String::toUpperCase).forEach(System.out::println);
+//
+//        //Given a List of integers, write a program to filter out the even numbers using streams.
+//        // Count the number of elements in a list that satisfy a specific condition using streams.
+//
+//        nums.stream().filter(n -> n % 2 == 0).forEach(System.out::println);
+//        int even = (int) nums.stream().filter(n -> n % 2 == 0).count();
+//        System.out.println("no of even "+even);
+//
+//
+//        //Write a program to find the average of a List of floating-point numbers using streams.
+//        List<Double> numsFloat = Arrays.asList(1.3, 2.4, 3.1, 4.2, 5.4);
+//        double avg = numsFloat.stream().mapToDouble(Double::doubleValue).average().getAsDouble();
+//        System.out.println("avg ::"+avg);
+//
+//        //Given a List of strings, write a program to concatenate all the strings using streams.
+//        String concat = fruits.stream().collect(Collectors.joining());
+//        System.out.println(concat);
+//
+//        //Write a program to remove duplicate elements from a List using streams.
+//        fruits.stream().distinct().forEach(System.out::println);
+//
+//        //Given a List of objects, write a program to sort the objects based on a specific attribute using streams.
+//        List<Person> people = Arrays.asList(
+//                new Person("Alice", 21),
+//                new Person("Anna", 29),
+//                new Person("Bob", 30),
+//                new Person("Barbie", 56),
+//                new Person("Charlie", 20)
+//        );
+//
+//        people.stream().sorted(Comparator.comparing(Person::getAge)).collect(Collectors.toList()).forEach(System.out::println);
+//
+//        //Write a program to check if all elements in a List satisfy a given condition using streams.
+//        List<Integer> numbers = Arrays.asList(2, 4, 6, 8, 10);
+//        boolean allEven = numbers.stream().allMatch(n -> n % 2 == 0);
+//        System.out.println("All numbers are even: " + allEven);
+//
+//
+//        //Sort a list of strings in ascending order using streams.
+//        fruits.stream().sorted().collect(Collectors.toList()).forEach(System.out::println);
+//
+//        //Check if a list contains a specific element using streams.
+//        boolean exists = nums.stream().anyMatch(n -> n.equals(9));
+//        System.out.println(exists);
+//
+//        //Create a new list containing the square of each element from the original list using streams
+//        nums.stream().map(n -> n*n).collect(Collectors.toList()).forEach(System.out::println);
+//
+//        //Find the average length of strings in a list using streams.
+//        fruits.stream().mapToInt(String::length).average();
+//
+//
+//        //GROUP by DEPT and find max salary
+//        List<Employee> employees = new ArrayList<>();
+//        employees.add(new Employee("Alice", "HR", 50000.0));
+//        employees.add(new Employee("Bob", "IT", 60000.0));
+//        employees.add(new Employee("Charlie", "Finance", 55000.0));
+//        employees.add(new Employee("David", "IT", 70000.0));
+//        employees.add(new Employee("Eva", "HR", 45000.0));
+//        employees.add(new Employee("Frank", "Finance", 58000.0));
+////
+//        Map<String, Optional<Employee>> highestSalaryPerDept = employees.stream()
+//                .collect(Collectors.groupingBy(
+//                        Employee::getDepartment,
+//                        Collectors.maxBy(Comparator.comparingDouble(Employee::getSalary))
+//                ));
+//
+//
+//
+//        //Find the longest string in a list using streams.
+//        fruits.stream().mapToInt(String::length).max();
+//
+////        Group a list of objects based on a specific attribute using streams.
+////        List<Employee> sortedEmployees = people.stream()
+////                .sorted(Comparator.comparing(Employee::getDepartment)
+////                        .thenComparing(Employee::getSalary))
+////                .collect(Collectors.toList());
+//
+//
+//        //Remove null values from a list using streams.
+//        List<String> nonNullValues = fruits.stream()
+//                .filter(Objects::nonNull)
 //                .collect(Collectors.toList());
-
-
-        //Remove null values from a list using streams.
-        List<String> nonNullValues = fruits.stream()
-                .filter(Objects::nonNull)
-                .collect(Collectors.toList());
-
-
-        //Find the second smallest element in a list of integers using streams.
-        Optional<Integer> secondSmallest = numbers.stream()
-                .distinct()
-                .sorted()
-                .skip(1)
-                .findFirst();
-
-
-
-        //Find the intersection of two lists using streams
-        List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5);
-        List<Integer> list2 = Arrays.asList(4, 5, 6, 7, 8);
-
-        List<Integer> intersection = list1.stream()
-                .filter(list2::contains)
-                .collect(Collectors.toList());
-
-
-    }
-}
+//
+//
+//        //Find the second smallest element in a list of integers using streams.
+//        Optional<Integer> secondSmallest = numbers.stream()
+//                .distinct()
+//                .sorted()
+//                .skip(1)
+//                .findFirst();
+//
+//        //Find the intersection of two lists using streams
+//        List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5);
+//        List<Integer> list2 = Arrays.asList(4, 5, 6, 7, 8);
+//
+//        List<Integer> intersection = list1.stream()
+//                .filter(list2::contains)
+//                .collect(Collectors.toList());
+//
+//        //Find frequency of every character in a string
+//
+//        String name = "Varsha";
+//
+//        Map<Character, Long> collect = name.chars().mapToObj(c -> (char) c).
+//                collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+//
+//        System.out.println(collect);
+//        String inputString = "ilovecoding";
+//
+//        Map<Character, Long> charCountMap = inputString.chars()
+//                .mapToObj(c -> (char) c)
+//                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+//
+//        // Step 5: Filter the Map to keep only the characters with occurrence count greater than 1 (duplicates).
+//         charCountMap.entrySet().stream()
+//                .filter(entry -> entry.getValue() > 1)
+//                .map(Map.Entry::getKey)
+//                .collect(Collectors.toSet());
+//
+//         //Find first non repeating character
+//        Map<Character, Integer> UniquecharCountMap = new LinkedHashMap<>();
+//
+//        // Iterate through the characters and count their occurrences.
+//        for (char c : inputString.toCharArray()) {
+//            UniquecharCountMap.put(c, UniquecharCountMap.getOrDefault(c, 0) + 1);
+//        }
+//
+//        Character character = inputString.chars().mapToObj(c -> (char) c).filter(e -> UniquecharCountMap.get(e) == 1).findFirst().get();
+//        System.out.println(character);
+//
+//
+//        //find second highest number
+//        int[] arr = {2,3,8,9,10,21,12};
+//        int maxTotal = Arrays.stream(arr).max().orElse(Integer.MIN_VALUE);
+//
+//        // Find the second highest number by excluding the maximum value.
+//        int secondHighest = Arrays.stream(arr)
+//                .filter(num -> num < max)
+//                .max()
+//                .orElse(Integer.MIN_VALUE);
+//
+//
+//
+//    }
+//}
